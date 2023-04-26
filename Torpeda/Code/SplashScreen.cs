d3472eaa37314383b946fd172942aeb1e931c184
Viktor <viktor123456789m@mail.ru>
2023-04-27 00:36:03 +0500

    1 загрузка

---
+++ Torpeda/Code/SplashScreen.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Torpeda;
+
+static class SplashScreen
+{
+    public static Texture2D menu { get; set; }
+    static int TimeCounter = 0;
+    static Color color;
+    public static SpriteFont Font { get; set; }
+    static Vector2 TextPosition = new Vector2(900, 100);
+
+    static public void Draw(SpriteBatch spriteBatch)
+    {
+        spriteBatch.Draw(menu, new Rectangle(0, 0, 1920, 1080), Color.White);
+        spriteBatch.DrawString(Font, "Торпеда!", TextPosition, color);
+
+    }
+
+    static public void Update()
+    {
+        color = Color.FromNonPremultiplied(255, 255, 255, TimeCounter % 256);
+        TimeCounter++;
+    }
+}
